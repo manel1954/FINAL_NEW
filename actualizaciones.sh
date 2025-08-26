1848f369393167fc8d31bce402beff5b04f16e19
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-08-26 18:58:13 +0200

    Update actualizaciones.sh

--- actualizaciones.sh
+++ actualizaciones.sh
@@ -4,7 +4,7 @@
 REPO_DIR="/home/pi/ACTUALIZACIONES_FINAL_NEW"
 STATUS_FILE="/home/pi/status.ini"
 ACTUALIZACIONES_TXT="$REPO_DIR/actualizaciones.txt"
-FORM_SCRIPT="/home/pi/A108/qt/./qt_actualizaciones"  # Ruta al script que abre tu formulario Qt
+FORM_SCRIPT="/home/pi/A108/qt/./qt_actualizacion"  # Ruta al script que abre tu formulario Qt
 
 # Cambiar al directorio
 cd "$REPO_DIR" || {
