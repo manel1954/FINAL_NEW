d39664e940266f42e343395e3c512dc8d415fd13
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-08-27 16:03:07 +0200

    Update actualizar_ids.sh

--- actualizar_ids.sh
+++ actualizar_ids.sh
@@ -1,26 +1,5 @@
 ﻿#!/bin/bash
 
-#var=`grep -n -m 1 '\<DMRIDPATH\>' /home/pi/MMDVMHost/linux/DMRIDUpdate.sh` 
-#buscar=":"
-#largo_linea=`expr index $var $buscar`
-#largo_linea=`expr $largo_linea - 1`
-#numero_linea=`expr substr $var 1 $largo_linea`
-#letrac=c
-#linea_sed=$numero_linea$letrac
-#sed -i "$linea_sed DMRIDPATH=/home/pi/MMDVMHost" /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
-#
-## DMR IDs now served by RadioID.net
-#var=`grep -n -m 1 '\<DATABASEURL\>' /home/pi/MMDVMHost/linux/DMRIDUpdate.sh`
-#buscar=":"
-#largo_linea=`expr index $var $buscar`
-#largo_linea=`expr $largo_linea - 1`
-#numero_linea=`expr substr $var 1 $largo_linea`
-#letrac=c
-#linea_sed=$numero_linea$letrac
-#sed -i "$linea_sed DATABASEURL='https://ham-digital.org/status/users.csv'" /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
-#
-#sudo sh /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
-
                         #14-08-2020 cambio actualizar para que salgan los indicativos en DVSWITCH:
                         cd /var/lib/mmdvm
                         sudo curl --fail -o DMRIds.dat -s http://www.pistar.uk/downloads/DMRIds.dat
